47210be9cb9a905b1de9ef91cccf0ca8fac43de7
judismar <solid.judis@gmail.com>
2023-11-10 09:43:41 -0300

    Add files via upload

--- tipo-lista/lista.c
+++ tipo-lista/lista.c
@@ -1,1 +1,43 @@
+#include "lista.h"
+#include <stdio.h>
+#include <stdlib.h>
+#define TAMANHO_INICIAL_LISTA 8
 
+struct lista
+{
+    int* a;
+    int n;
+};
+
+Lista* criaLista()
+{
+    Lista* l = (Lista*) malloc(sizeof(Lista));
+    l->n = 0; //(*l).n = 0;
+    l->a = (int*) malloc(TAMANHO_INICIAL_LISTA*sizeof(int));
+    return l;
+}
+
+void insere(Lista *l, int num)
+{
+    if(l->n == TAMANHO_INICIAL_LISTA)
+    {
+        printf("Overflow.\n");
+        return;
+    }
+        //realocar array, pois deu overflow.
+    l->a[l->n] = num;
+    l->n ++;
+}
+
+void printLista(Lista *l)
+{
+    for(int k = 0; k < l->n; k++)
+        printf("%d ", l->a[k]); //(*l).*(a+k)
+    printf("\n");
+}
+
+void liberaLista(Lista *l)
+{
+    free(l->a);
+    free(l);
+}
---
+++ tipo-lista/lista.h
@@ -0,0 +1,6 @@
+typedef struct lista Lista;
+
+Lista* criaLista();
+void insere(Lista*, int);
+void printLista(Lista*);
+void liberaLista(Lista*);
